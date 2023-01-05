071faae01c05a7bffd51bb04cd51524d753b34af
jfva01 <jvargasascanio@hotmail.com>
2023-01-04 23:35:43 -0300

    1.- Mostramos la lista de productos registrados en nuestra tabla 2.- Agregamos la funcionalidad de validar los campos al momento de guardar un producto. 3.- Restringimos el tipo de formato ingresado en el campo de precio con jquery.validate

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -17,6 +17,7 @@ namespace CapaPresentacionAdmin
                         "~/Scripts/DataTables/dataTables.responsive.js",
                         "~/Scripts/loadingoverlay/loadingoverlay.min.js",
                         "~/Scripts/sweetalert2.all.min.js",
+                        "~/Scripts/jquery.validate.js",
                         "~/Scripts/scripts.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
